b63b1941384c9eb668b816fcaaaedc564d6d0ed6
Benjamin Falk <bn_falk@yahoo.com>
2015-06-11 16:48:11 -0500

    Adding in JWT lib that will be needed for the id_token

--- mix.exs
+++ mix.exs
@@ -34,6 +34,7 @@ defmodule Ganondorf.Mixfile do
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 1.0"},
      {:phoenix_live_reload, "~> 0.4", only: :dev},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:joken, github: "bryanjos/joken"}]
   end
 end
